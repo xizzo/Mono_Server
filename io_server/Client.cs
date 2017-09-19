0e9ffe0820906495287019a7e21a08784e6e22c9
Johnny Wuytack <johnny.wuytack@be.dsv.com>
2017-09-19 13:13:50 +0200

    Initial commit

---
+++ io_server/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace io_server
+{
+    class Client
+    {
+      public TcpClient clientSocket { get; set; }
+
+      public Client(TcpClient clientSocket)
+      {
+         this.clientSocket = clientSocket;
+      }
+
+   }
+}
---
+++ io_server/io_server.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Collections.Generic;
+
+namespace io_server
+{
+   class io_server
+   {
+
+      static Int32 serverPort = 7777;
+
+      static TcpListener serverSocket;
+      static TcpClient clientSocket;
+
+      static Boolean serverIsActive = false;
+
+      static List<Client> lstClients = new List<Client>();
+
+      static void Main(string[] args)
+      {
+         try
+         {
+            StartServer();
+            ServerThread().Wait();
+         }
+         catch (Exception)
+         {
+            throw;
+         }
+      }
+
+      static void StartServer()
+      {
+         serverIsActive = true;
+
+         clientSocket = default(TcpClient);
+
+         serverSocket = new TcpListener(new IPEndPoint(IPAddress.Parse("127.0.0.1"), serverPort));
+         serverSocket.Start();
+      }
+
+      static async System.Threading.Tasks.Task ServerThread()
+      {
+         while (serverIsActive)
+         {
+            clientSocket = await serverSocket.AcceptTcpClientAsync();
+            Client oClient = new Client(clientSocket);
+            lstClients.Add(oClient);
+         }
+      }
+
+   }
+}
